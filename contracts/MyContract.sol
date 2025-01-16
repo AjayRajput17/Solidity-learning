0a5ac32a9e731b98559158ef3ad69339d8f47137
AjayRajput17 <ajayingle3878@gmail.com>
2025-01-16 22:55:57 +0530

    initial commit

---
+++ contracts/MyContract.sol
@@ -0,0 +1,14 @@
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract MyContract {
+    // state variable
+    // Local Variable
+
+    function getValue() public {
+        
+    }
+}
