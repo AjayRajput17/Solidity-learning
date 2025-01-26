4386dcc2020854059ce35d09fb832e75ded6ead3
AjayRajput17 <ajayingle3878@gmail.com>
2025-01-26 19:06:00 +0530

    hotel booking

--- contracts/Function1.sol
+++ contracts/Function1.sol
@@ -46,3 +46,5 @@ contract Function {
         }
     
 }
+
+    
---
+++ contracts/HotelRoom.sol
@@ -0,0 +1,50 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HotelRoom {
+
+    // vacant
+    // Occupied
+    enum Statuses { 
+        Vacant, Occupied
+    }
+
+    Statuses  public currentStatus;
+
+    event Occupy(address _occpant, uint _value);
+
+    address payable public owner;
+      
+
+
+    constructor() {
+        owner = payable (msg.sender);        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant {
+        //check status
+         require(currentStatus == Statuses.Vacant, "Currently Occupied.");
+         _;
+    }
+
+    modifier costs(uint _amount) {
+        // check price
+        require(msg.value >= _amount, "Not enough ether provided.");
+        _;
+    }
+
+    function book() public payable  onlyWhileVacant costs(2 ether){ 
+        currentStatus = Statuses.Occupied;
+
+        owner.transfer(msg.value);
+        (bool sent, bytes memory data) =  owner.call{value: msg.value}("");
+        require(true);
+
+        emit Occupy(msg.sender, msg.value);
+    }
+
+
+
+
+}
